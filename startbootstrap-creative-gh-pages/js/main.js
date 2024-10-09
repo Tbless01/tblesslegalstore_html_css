df7639389d3a0e19e30396b7d746c77fd62035ee
Tbless01 <olaitanblessinge@gmail.com>
2024-10-09 12:56:10 +0100

    google login update

--- startbootstrap-creative-gh-pages/js/main.js
+++ startbootstrap-creative-gh-pages/js/main.js
@@ -14,8 +14,22 @@ function onSignIn(response) {
     $("#image").attr('src', responsePayload.picture);
     
     // Show user data and hide sign-in button
-    $(".data").css("display", "block");
-    $(".g_id_signin").css("display", "none");
+    // $(".data").css("display", "block");
+    // $(".g_id_signin").css("display", "none");
+
+    const customerRequest = {
+        email: responsePayload.email,
+        firstName: firstName,
+        lastName: lastName
+        // Optionally include other user info if needed
+    };
+
+    // Save the selected package code
+    localStorage.setItem('selectedPackageCode', packageCode);
+
+    // Authenticate the customer
+    authenticateGoogleCustomer(customerRequest);
+    window.location.href = 'customers-list.html'; // Redirect after login
 }
 
 // Decode JWT token returned by Google
@@ -36,4 +50,28 @@ function signOut() {
     $(".data").css("display", "none");
 
     alert('User signed out successfully.');
+}
+
+
+
+async function authenticateGoogleCustomer(customerRequest) {
+    try {
+        const response = await fetch(`${BASE_URL}/authentication/google-customer`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(customerRequest)
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.json();
+        console.log('Authentication successful:', responseData);
+        // Handle successful authentication (e.g., redirect or show a success message)
+    } catch (error) {
+        console.error('Error authenticating Google customer:', error);
+    }
 }
